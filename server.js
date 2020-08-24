5a8f96be2cfd6ecfe3c2af176ed49f34b2bbef65
Youzhi Hu <jyzhu0101@gmail.com>
2020-08-24 19:20:19 +1000

    Setup passport, express-session & api-routes.

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Require necessary npm packages
 const express = require("express");
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -11,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static("./public/"));
 
 // Set Handlebars.
 const exphbs = require("express-handlebars");
@@ -19,8 +22,16 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Requiring our routes
 require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
