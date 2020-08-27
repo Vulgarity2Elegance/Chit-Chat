18aafa5ead2506483dc03ad2d313cb7943114d5d
Youzhi Hu <jyzhu0101@gmail.com>
2020-08-27 11:22:06 +1000

    Added One-to-Many association with UserRecipe Model.

--- models/user.js
+++ models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Ingredient);
+      User.hasMany(models.UserRecipe);
     }
   }
   User.init(
