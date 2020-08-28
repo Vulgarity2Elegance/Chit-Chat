b1487182b3048576505fd89e35b4463242a3c885
Youzhi Hu <jyzhu0101@gmail.com>
2020-08-28 12:06:38 +1000

    Rendered card for searched ingredients.

--- public/js/member.js
+++ public/js/member.js
@@ -7,53 +7,24 @@ $(document).ready(() => {
 
   $("#searchButton").on("click", (e) => {
     e.preventDefault();
-    searchRecipe();
+    searchedIngredients();
   });
 
-  function searchRecipe() {
-    const searchedIngredients = $("#ingredients").val();
-    $("#button1").text(searchedIngredients);
-  }
-});
+  $(".searchedIngredients").on("click", ".ingredient", function () {
+    // Search related recipe for searched ingredient.
+    const query = $(this).data("name");
+    $.ajax({
+      url:
+        "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
+        query +
+        "&apiKey=27eccb2b8d6b4c8d99d5512e94ae0884",
+      method: "GET",
+    }).then((data) => {
+      console.log(data);
 
-$(".recipe-card").on("click", ".instruction", function () {
-  const id = $(this).data("id");
-  $.ajax({
-    url:
-      "https://api.spoonacular.com/recipes/" +
-      id +
-      "/analyzedInstructions?&apiKey=27eccb2b8d6b4c8d99d5512e94ae0884",
-    method: "GET",
-  }).then((response) => {
-    console.log(response[0].steps);
-    const data = response[0].steps;
-    const templates = [];
-    data.forEach((item) => {
-      templates.push(`
-      <ul class="list-group">
-        <li class="list-group-item">${item.number}.${item.step}</li>
-      </ul>
-      `);
-    });
-    $(".modal-body").html(templates);
-  });
-});
-
-$("#button1").on("click", (e) => {
-  e.preventDefault();
-  $.ajax({
-    url:
-      "https://api.spoonacular.com/recipes/findByIngredients?" +
-      "ingredients=" +
-      $("#button1").text() +
-      "&apiKey=27eccb2b8d6b4c8d99d5512e94ae0884",
-    method: "GET",
-  }).then((data) => {
-    console.log(data);
-
-    const templates = [];
-    data.forEach((item) => {
-      templates.push(`
+      const templates = [];
+      data.forEach((item) => {
+        templates.push(`
       <div class="card-deck">
         <div class="card">
           <img class="card-img-top" src="${item.image}" alt="Card image cap" />
@@ -62,12 +33,54 @@ $("#button1").on("click", (e) => {
             <p class="card-text">
               ${item.id}
             </p>
-            <button class="instruction btn btn-primary" data-id="${item.id}" data-toggle="modal" data-target="#exampleModalCenter">View Instructions!</button>
+            <button class="instruction btn btn-primary btn-sm" data-id="${item.id}" data-toggle="modal" data-target="#exampleModalCenter">View Instructions!</button>
+            <button class="recipeID btn btn-success btn-sm ml-3">Save Recipe!</button>
           </div>
         </div>
       </div>
     `);
+      });
+      $(".recipe-card").html(templates);
     });
-    $(".recipe-card").html(templates);
+  });
+
+  function searchedIngredients() {
+    // Rendering a card for an ingredient in order to search recipe or save the ingredient.
+    const searchedIngredients = $("#ingredients").val();
+    const templates = [];
+    templates.push(`
+  <div class="card text-white bg-warning mb-3">
+    <div class="card-body">
+      <h5 class="card-title text-center">${searchedIngredients}</h5>
+      <button class="ingredient btn btn-primary btn-sm" data-name="${searchedIngredients}">Search Recipes!</button>
+      <button class="btn btn-success btn-sm">Save Ingredient!</button>
+    </div>
+  </div>
+  `);
+    $(".searchedIngredients").html(templates);
+  }
+
+  $(".recipe-card").on("click", ".instruction", function () {
+    // Redering instructions for the selected recipe
+    const id = $(this).data("id");
+    $.ajax({
+      url:
+        "https://api.spoonacular.com/recipes/" +
+        id +
+        "/analyzedInstructions?&apiKey=27eccb2b8d6b4c8d99d5512e94ae0884",
+      method: "GET",
+    }).then((response) => {
+      console.log(response[0].steps);
+      const data = response[0].steps;
+      const templates = [];
+      data.forEach((item) => {
+        templates.push(`
+      <ul class="list-group">
+        <li class="list-group-item">${item.number}.${item.step}</li>
+      </ul>
+      `);
+      });
+      $(".modal-body").html(templates);
+    });
   });
 });
